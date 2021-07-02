ff97e8b1ae3fb2f2994d6a418e675e1ba34bc514
Stefani Troya <josselyn.troya@epn.edu.ec>
2021-07-02 09:28:06 -0500

    firestore-crud

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,26 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
+  /* {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+  }, */
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'create-task',
     pathMatch: 'full'
+  },
+  {
+    path: 'create-task',
+    loadChildren: () => import('./create-todo/create-todo.module').then( m => m.CreateTodoPageModule)
+  },
+  {
+    path: 'todo-list',
+    loadChildren: () => import('./todo-list/todo-list.module').then( m => m.TodoListPageModule)
+  },
+  {
+    path: 'update-todo/:id',
+    loadChildren: () => import('./update-todo/update-todo.module').then( m => m.UpdateTodoPageModule)
   },
 ];
 
